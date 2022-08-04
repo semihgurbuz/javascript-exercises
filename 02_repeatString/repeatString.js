96073a5f6152a42c9ca6b8ef564b7bf68ea4f2e4
semihgurbuz <semihgurbuz.dev@gmail.com>
2022-08-04 20:01:40 +0300

    done the helloWorld and repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (word, number) {
+  let array = [];
+  if (number >= 0 && word.length != 0) {
+    for (i = 0; i < number; i++) {
+      array.push(word);
+    }
+  } else if (number >= 0 && word.length === 0) {
+    array = [];
+  } else {
+    array = ["ERROR"];
+  }
 
+  return array.join("");
 };
 
 // Do not edit below this line
