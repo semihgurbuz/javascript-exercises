2d958b9dda36d18df38c0a131d15028d070793af
semihgurbuz <semihgurbuz.dev@gmail.com>
2022-08-09 19:18:06 +0300

    finished fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...number) {
+  for (i = 0; i < number.length; i++) {
+    const index = array.indexOf(number[i]);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+  }
 
+  return array;
 };
 
 // Do not edit below this line
