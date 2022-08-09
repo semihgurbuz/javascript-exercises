48f3e5e8e79cd802059d21e5f65e254f957e5aa1
semihgurbuz <semihgurbuz.dev@gmail.com>
2022-08-09 21:35:48 +0300

    finished all part 4 exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,36 @@
-const sumAll = function() {
-
+const sumAll = function (n1, n2) {
+  let array = [n1, n2];
+  if (
+    n1 < n2 &&
+    n1 > 0 &&
+    n2 > 0 &&
+    typeof n1 == "number" &&
+    typeof n2 == "number"
+  ) {
+    while (n1 < n2 - 1) {
+      n2--;
+      array.push(n2);
+    }
+    const sum = array.reduce((a, b) => a + b, 0);
+    return sum;
+  } else if (
+    n1 > n2 &&
+    n1 > 0 &&
+    n2 > 0 &&
+    typeof n1 == "number" &&
+    typeof n2 == "number"
+  ) {
+    while (n2 < n1 - 1) {
+      n1--;
+      array.push(n1);
+    }
+    const sum = array.reduce((a, b) => a + b, 0);
+    return sum;
+    //   } else if (typeof n1 !== "number" || typeof n2 !== "number") {
+    //     return "ERROR";
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  let conv = ((temp - 32) * 5) / 9;
+  return Math.round(conv * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  let conv = (temp * 9) / 5 + 32;
+  return Math.round(conv * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
